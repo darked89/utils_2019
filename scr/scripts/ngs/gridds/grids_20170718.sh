848a04484dcf104354e1be4424b2a42283a23d79
Darek <darek.kedra@gumed.edu.pl>
2019-05-15 19:52:09 +0200

    Update /scr/scripts/ngs/gridds/grids_20170718.sh

---
+++ scr/scripts/ngs/gridds/grids_20170718.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Example gridss pipeline for single sample analysis
+#
+
+## 20190515 DK: located /DANE_SYNOLOGY/users/arek/Project_data/DoD_2015/GRIDSS/analysis/gridss.sh
+
+INPUT=${1}
+BLACKLIST=/opt/GRIDSS/hg38.blacklist.bed
+REFERENCE=/opt/GRIDSS/hg38canon.fa
+OUTPUT=${INPUT/.bam/.sv.vcf}
+ASSEMBLY=${OUTPUT/.sv.vcf/.gridss.assembly.bam}
+GRIDSS_JAR=/opt/GRIDSS/bin/gridss-1.4.1-jar-with-dependencies.jar
+
+if [[ ! -f "$INPUT" ]] ; then
+	echo "Missing $INPUT input file."
+	exit 1
+fi
+if ! which bwa >/dev/null 2>&1 ; then
+	echo "Missing bwa. Please add to PATH"
+	exit 1
+fi
+if [[ ! -f "$REFERENCE" ]] ; then
+	echo "Missing reference genome $REFERENCE. Update the REFERENCE variable in the shell script to your hg19 location"
+	echo "For the example file chr12.1527326.DEL1024.bam, ReorderSam can be used to match to your version of hg19. In the case of this example, only \"chr12\" is required to exist, and difference in alternate contigs can be ignored (using ALLOW_INCOMPLETE_DICT_CONCORDANCE=true)."
+	echo "For real data, please ensure that all BAM files are aligned to the same reference, and the reference supplied to GRIDSS matches that used for alignment."
+	exit 1
+fi
+if [[ ! -f "$REFERENCE.bwt" ]] ; then
+	echo "Missing bwa index for $REFERENCE. Could not find $REFERENCE.bwt. Create a bwa index (using \"bwa index $REFERENCE\") or symlink the index files to the expected file names."
+	exit 1
+fi
+if [[ ! -f $GRIDSS_JAR ]] ; then
+	echo "Missing $GRIDSS_JAR. Update the GRIDSS_JAR variable in the shell script to your location"
+	exit 1
+fi
+if ! which java >/dev/null 2>&1 ; then
+	echo "Missing java. Please add java 1.8 or later to PATH"
+	exit 1
+fi
+JAVA_VERSION="$(java -version 2>&1 | head -1)"
+if [[ ! "$JAVA_VERSION" =~ "\"1.8" ]] ; then
+	echo "Detected $JAVA_VERSION. GRIDSS requires Java 1.8 or later."
+	exit 1
+fi
+
+java -ea -Xmx31g \
+	-Dsamjdk.create_index=true \
+	-Dsamjdk.use_async_io_read_samtools=true \
+	-Dsamjdk.use_async_io_write_samtools=true \
+	-Dsamjdk.use_async_io_write_tribble=true \
+	-cp $GRIDSS_JAR gridss.CallVariants \
+	TMP_DIR=. \
+	WORKING_DIR=. \
+	REFERENCE_SEQUENCE="$REFERENCE" \
+	INPUT="$INPUT" \
+	OUTPUT="$OUTPUT" \
+	ASSEMBLY="$ASSEMBLY" \
+	BLACKLIST="$BLACKLIST" \
+	2>&1 | tee -a gridss.$HOSTNAME.$$.log
